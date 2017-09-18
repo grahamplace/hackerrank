e480a27808eb044ac19ceaf6eea51e394b07c090
Graham Place <grahamgplace@gmail.com>
2017-09-17 17:48:25 -0700

    add leap year python

---
+++ python/leap_year.py
@@ -0,0 +1,12 @@
+def is_leap(year):
+    leap = False
+    # The year can be evenly divided by 4, is a leap year, unless:
+    if year % 4 == 0:
+        leap = True
+        # The year can be evenly divided by 100, it is NOT a leap year, unless:
+        if year % 100 == 0:
+            leap = False
+            # The year is also evenly divisible by 400. Then it is a leap year.
+            if year % 400 == 0:
+               leap = True
+    return leap
